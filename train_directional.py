847e368d30dc29d7594afd76bba025366166c823
Ethan Chen <gradschool.hchen13@gmail.com>
2018-06-13 15:18:14 +0800

    save model during training when notable improvements occur

--- train_directional.py
+++ train_directional.py
@@ -1,5 +1,9 @@
+import keras
+
 from models import future_direction_conv
 import numpy as np
+
+from settings import *
 
 
 def make_labels(x, y):
@@ -7,18 +11,36 @@ def make_labels(x, y):
     return price_diff > 0
 
 
+class SaveModel(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        acc = int(logs['val_acc'] * 100)
+        if acc > 60 or (self.accepted_acc is not None and acc - self.accepted_acc >= 1):
+            self.model.save('assets/directional_{}.h5'.format(acc))
+            self.accepted_acc = acc
+
+
 if __name__ == '__main__':
     x_train = np.load('cache/x_train.npy')
     y_raw = np.load('cache/y_train.npy')
     y_train = make_labels(x_train, y_raw)
+    # x_train = x_train[:1000]
+    # y_train = y_train[:1000]
 
     x_valid = np.load('cache/x_test.npy')
     y_valid = np.load('cache/y_test.npy')
     y_valid = make_labels(x_valid, y_valid)
+    # x_valid = x_valid[:1000]
+    # y_valid = y_valid[:1000]
+
+    ensure_dir_exists(os.path.join(ROOT_DIR, 'assets'))
 
     model = future_direction_conv(x_train.shape)
+
+    cb_save = SaveModel()
+
     train_history = model.fit(
-        x_train, y_train, epochs=1, batch_size=128, shuffle=True,
-        validation_data=(x_valid, y_valid)
+        x_train, y_train, epochs=50, batch_size=128, shuffle=True,
+        validation_data=(x_valid, y_valid),
+        callbacks=[cb_save]
     )
     print("\nTraining complete!\n")
